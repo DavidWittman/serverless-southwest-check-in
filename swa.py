9a251552f627cf6da3484c6279a2c5e5225d5b60
David Wittman <dwittman@gmail.com>
2016-08-22 20:49:07 -0500

    First commit

---
+++ swa.py
@@ -0,0 +1,65 @@
+import requests
+
+BASE_URL = "https://api-extensions.southwest.com/v1/mobile"
+USER_AGENT = "Southwest/3.3.7 (iPhone; iOS 9.3; Scale/2.00)"
+API_KEY = "l7xx8d8bfce4ee874269bedc02832674129b"
+
+
+class ReservationError(Exception):
+    pass
+
+
+class CheckInError(Exception):
+    pass
+
+
+def _make_request(path, data, content_type, check_status_code=True):
+    url = "%s%s" % (BASE_URL, path)
+    headers = {
+        "User-Agent": USER_AGENT,
+        "Content-Type": content_type,
+        "X-Api-Key": API_KEY,
+        "Accept-Language": "en-US;q=1"
+    }
+
+    response = requests.post(url, json=data, headers=headers, verify=False)
+
+    if check_status_code and not response.ok:
+        try:
+            msg = response.json()['message']
+        except:
+            msg = response.reason
+
+        raise SouthWestAPIError("status_code=%s msg=\"%s\"" % (response.status_code, msg))
+
+    return response
+
+# TODO(dw): Create a handler wrapper to accept event/context?
+
+def get_reservation(event, context):
+    first_name = event['first_name']
+    last_name = event['last_name']
+    confirmation_number = event['confirmation_number']
+
+def check_in(event, context):
+    content_type = "application/vnd.swacorp.com.mobile.boarding-passes-v1.0+json"
+    first_name = event['first_name']
+    last_name = event['last_name']
+    confirmation_number = event['confirmation_number']
+    data = {
+        'names': [{
+            'firstName': first_name,
+            'lastName': last_name
+        }]
+    }
+
+    response = _make_request(
+        "/reservations/record-locator/%s/boarding-passes" % confirmation_number,
+        data,
+        content_type
+    )
+
+    check_in_docs = response.json()['passengerCheckInDocuments'][0]['checkinDocuments']
+
+    message = "confirmation_number=%s documents=%s emailed=false" % (confirmation_number, len(check_in_docs))
+    return dict(message=message, event=event)
---
+++ test_swa.py
@@ -0,0 +1,79 @@
+import unittest
+
+import mock
+import responses
+
+import swa
+
+
+class TestRequest(unittest.TestCase):
+
+    @mock.patch('swa.requests')
+    def test_make_request_call(self, mock_requests):
+        expected_headers = {
+            "User-Agent": "Southwest/3.3.7 (iPhone; iOS 9.3; Scale/2.00)",
+            "Content-Type": "application/vnd.swacorp.com.mobile.boarding-passes-v1.0+json",
+            "X-Api-Key": "l7xx8d8bfce4ee874269bedc02832674129b",
+            "Accept-Language": "en-US;q=1"
+        }
+        expected_url = "https://api-extensions.southwest.com/v1/mobile/foo/123456/bar"
+        fake_data = {}
+
+        _ = swa._make_request("/foo/123456/bar", fake_data, content_type="application/vnd.swacorp.com.mobile.boarding-passes-v1.0+json")
+
+        mock_requests.post.assert_called_with(expected_url, json=fake_data, headers=expected_headers, verify=False)
+
+class TestCheckIn(unittest.TestCase):
+
+    def setUp(self):
+        self.event = dict(first_name="George", last_name="Bush", confirmation_number="B2B8TG")
+        self.context = "fake context"
+        self.data = {
+            'names': [{
+                'firstName': 'George',
+                'lastName': 'Bush'
+            }]
+        }
+        # TODO(dw): Complete this fixture and move it elsewhere in the test suite
+        self.successful_check_in_response = {
+            'maxFailedCheckInAttemptsReached': False,
+            'passengerCheckInDocuments': [{
+                'passenger': {
+                 },
+                 'checkinDocuments': [{
+                        'boardingGroupNumber': '01',
+                        'boardingGroup': 'A',
+                        'documentType': 'BOARDING_PASS',
+                        'origin': 'AUS',
+                        'destination': 'LAS',
+                        'flightNumber': '4242'
+                 }]
+            }]
+        }
+
+    @mock.patch('swa._make_request')
+    def test_check_in_call(self, mock_make_request):
+        swa.check_in(self.event, self.context)
+        mock_make_request.assert_called_with(
+                "/reservations/record-locator/B2B8TG/boarding-passes",
+                self.data,
+                "application/vnd.swacorp.com.mobile.boarding-passes-v1.0+json"
+        )
+
+    @responses.activate
+    def test_check_in_success(self):
+        responses.add(
+            responses.POST,
+            'https://api-extensions.southwest.com/v1/mobile/reservations/record-locator/B2B8TG/boarding-passes',
+            json=self.successful_check_in_response,
+            status=200
+        )
+        result = swa.check_in(self.event, self.context)
+        assert result['message'] == "confirmation_number=%s documents=1 emailed=false" % self.event['confirmation_number']
+        assert result['event'] == self.event
+
+
+class TestReservation(unittest.TestCase):
+
+    def test_get_reservation(self):
+        pass
