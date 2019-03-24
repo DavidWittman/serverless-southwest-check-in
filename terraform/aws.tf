546dfa8fd3590442b2e912da846ec7fafd770640
David Wittman <dwittman@gmail.com>
2019-03-24 10:43:44 -0500

    [terraform] Remove alias from provider

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -1,10 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-  alias = "region"
 }
 
 data "aws_caller_identity" "current" {}
 
-data "aws_region" "current" {
-  provider = "aws.region"
-}
+data "aws_region" "current" {}
